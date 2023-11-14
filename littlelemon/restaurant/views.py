7096585cff4d2f95568cf9f1db7a0f5802254333
Maksat Ylyasov <maxintelinc@gmail.com>
2023-11-14 21:29:31 +0300

    added the url configrutations:

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .serializers import MenuItemSerializer, BookingSerializer
+from .models import Booking
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    # queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    # queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
